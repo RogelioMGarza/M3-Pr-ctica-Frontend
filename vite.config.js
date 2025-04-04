21b62072e8de3a003512a69b6910438b62bbfd09
RogelioMGarza <a01722763@tec.mx>
2025-04-03 19:14:24 -0600

    dep

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
+// vite.config.js
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: '/M3-Pr-ctica-Frontend/', // ¡Este nombre debe coincidir con el repo!
   plugins: [react()],
 })
